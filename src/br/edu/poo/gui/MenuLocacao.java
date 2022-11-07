980130c600f14dc87221fa34ea774f33ee9024ed
Edelin Chaves <edelinchaves.dev@gmail.com>
2022-11-07 19:02:33 -0300

    Adição dos comentários no MenuLocacao.

--- src/br/edu/poo/gui/MenuLocacao.java
+++ src/br/edu/poo/gui/MenuLocacao.java
@@ -4,11 +4,13 @@ import br.edu.poo.objects.*;
 import java.util.Scanner;
 
 public class MenuLocacao {
+    // Criando um novo objeto da classe ListaLocacao, ListaVeiculo e ListaCliente.
     private ListaLocacao metodoLocacao = new ListaLocacao();
     private ListaCliente metodoCliente = new ListaCliente();
     private ListaVeiculo metodoVeiculo = new ListaVeiculo();
     Scanner input = new Scanner(System.in);
 
+    // Menu do programa.
     public void Menu() {
         int opcao = 0;
         do {
@@ -31,6 +33,7 @@ public class MenuLocacao {
 
             input.nextLine();
 
+            // Switch case para navegar pelo menu.
             switch (opcao) {
                 case 1:
                     addLocacao();
@@ -69,6 +72,7 @@ public class MenuLocacao {
         } while (opcao != 7);
     }
 
+    // Este método adiciona uma nova locação no sistema.
     public void addLocacao() {
         System.out.println("Digite o CPF do cliente: ");
         long cpf = input.nextLong();
@@ -97,32 +101,41 @@ public class MenuLocacao {
         } else {
             System.out.println("Opção inválida");
         }
+        // Criando um novo objeto da classe Locacao.
         Locacao locacao = new Locacao(cliente, veiculo, dataLocacao, dataDevolucao, seguro);
 
+        // Adicionando um novo objeto na lista de locações.
         metodoLocacao.add(locacao);
         System.out.println(locacao.getCodigo());
     }
 
+    // Este método pesquisa uma locação pelo código.
     public void pesquisarLocacao() {
         System.out.print("Digite o código da locação: ");
         int codigo = input.nextInt();
 
+        // Imprime o resultado da pesquisa pelo código.
         System.out.println(metodoLocacao.get(codigo));
     }
 
+    // Este método exibe as informações de uma locação.
     public void exibeInfoLocacao() {
         System.out.print("Digite o código da locação: ");
         int codigo = input.nextInt();
 
+        // Imprime o resultado da pesquisa pelo código.
         System.out.println(metodoLocacao.getInfo(codigo));
     }
 
+    // Este método exibe todas as locações cadastradas no sistema.
     public void dadosTodasLocacoes() {
         System.out.print("Aqui estão as informações de todas as locações: \n");
 
+        // Imprime o resultado do método que exibe todas as locações.
         System.out.println(metodoLocacao.getInfo());
     }
 
+    // Este método remove uma locação do sistema.
     public void removeLocacao() {
         System.out.print("Digite o código da locação: ");
         int codigo = input.nextInt();
@@ -139,10 +152,11 @@ public class MenuLocacao {
         }
     }
 
+    // Este método verifica se uma locação existe no sistema.
     public void existeLocacao() {
         System.out.print("Digite o código da locação: ");
         int codigo = input.nextInt();
-
+        // Imprime o resultado da pesquisa pelo código.
         System.out.println(metodoLocacao.existe(codigo));
     }
 }
