84efb55a8df55edae7625a5b89242b128bb02907
LuanEvander <luanevander.dev@gmail.com>
2022-11-07 06:23:32 -0300

    Refatoração

--- src/br/edu/poo/gui/Main.java
+++ src/br/edu/poo/gui/Main.java
@@ -14,11 +14,11 @@ public class Main {
         MenuCliente menuCliente = new MenuCliente();
         MenuLocacao menuLocacao = new MenuLocacao();
 
-        Veiculo veiculo1 = new Carro("ABC1234", "Fusca", 1970, 10.5F, 2, 4, false, 10);
-        Veiculo veiculo2 = new Carro("ABB1233", "Monza", 1999, 15.5F, 4, 5, true, 8);
-        Veiculo veiculo3 = new Carro("AAA1222", "Ferrari", 2010, 1000, 2, 2, true, 6.5F);
-        Veiculo veiculo4 = new Carro("AAC1111", "Santana", 1980, 50.2F, 4, 6, false, 10.5F);
-        Veiculo veiculo5 = new Carro("AAB1111", "Fiat 147", 1980, 50.7F, 2, 4, false, 10.5F);
+        Veiculo veiculo1 = new Carro("ABC-1234", "Fusca", 1970, 10.5F, 2, 4, false, 10);
+        Veiculo veiculo2 = new Carro("ABB-1233", "Monza", 1999, 15.5F, 4, 5, true, 8);
+        Veiculo veiculo3 = new Carro("AAA-1222", "Ferrari", 2010, 1000, 2, 2, true, 6.5F);
+        Veiculo veiculo4 = new Carro("AAC-1111", "Santana", 1980, 50.2F, 4, 6, false, 10.5F);
+        Veiculo veiculo5 = new Carro("AAB-1111", "Fiat 147", 1980, 50.7F, 2, 4, false, 10.5F);
 
         Cliente cliente1 = new Cliente(1234567890, "João", "Rua 1", 123456789, 987654321);
         Cliente cliente2 = new Cliente(1234567891, "Maria", "Rua 2", 123456789, 987654322);
@@ -32,27 +32,8 @@ public class Main {
         Locacao locacao4 = new Locacao(cliente4, veiculo4, "12/12/2022", "12/01/2023", true);
         Locacao locacao5 = new Locacao(cliente5, veiculo5, "12/12/2022", "12/01/2023", true);
 
-        listaVeiculos.add(veiculo1);
-        listaVeiculos.add(veiculo2);
-        listaVeiculos.add(veiculo3);
-        listaVeiculos.add(veiculo4);
-        listaVeiculos.add(veiculo5);
-
-        listaClientes.add(cliente1);
-        listaClientes.add(cliente2);
-        listaClientes.add(cliente3);
-        listaClientes.add(cliente4);
-        listaClientes.add(cliente5);
-        
-        listaLocacoes.add(locacao1);
-        listaLocacoes.add(locacao2);
-        listaLocacoes.add(locacao3);
-        listaLocacoes.add(locacao4);
-        listaLocacoes.add(locacao5);
-        
-
-        menuCliente.Menu();
-        //menuVeiculo.Menu();
+        //menuCliente.Menu();
+        menuVeiculo.Menu();
         //menuLocacao.Menu();
         
     }
