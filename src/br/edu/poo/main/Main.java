e0be5a77a15bbd689be87c42d792deed27ce417b
LuanEvander <luanevander.dev@gmail.com>
2022-11-07 08:09:57 -0300

    Realocação da Main visando melhor encapsulamento

---
+++ src/br/edu/poo/main/Main.java
@@ -0,0 +1,10 @@
+package br.edu.poo.main;
+
+import br.edu.poo.menu.MenuLocadora;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuLocadora menuLocadora = new MenuLocadora();
+        menuLocadora.Menu();
+    }
+}
