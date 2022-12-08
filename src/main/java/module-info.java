5b040d78eab710e8b98f214d22b8f8d588d1d360
LuanEvander <luanevander.dev@gmail.com>
2022-12-08 15:16:03 -0300

    Configuração de transitividade

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module br.edu.poo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens br.edu.poo.gui to javafx.fxml;
     exports br.edu.poo.gui;
