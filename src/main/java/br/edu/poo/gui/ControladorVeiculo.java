68f652eb17d71f295280420b992c3f610c3b27ca
LuanEvander <luanevander.dev@gmail.com>
2023-01-09 17:53:32 -0300

    Aoba

--- src/main/java/br/edu/poo/gui/ControladorVeiculo.java
+++ src/main/java/br/edu/poo/gui/ControladorVeiculo.java
@@ -1,8 +1,11 @@
 package br.edu.poo.gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import br.edu.poo.backend.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -10,7 +13,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class ControladorVeiculo {
+public class ControladorVeiculo implements Initializable{
     private ListaVeiculo metodoVeiculo = new ListaVeiculo();
 
     @FXML
@@ -34,13 +37,13 @@ public class ControladorVeiculo {
     @FXML
     private TextField mediaKm_gui;
     @FXML
-    private ChoiceBox<Object> arCondicionado_gui;
+    private ChoiceBox<String> arCondicionado_gui;
     private String[] arCondicionado = {"Sim", "Não"};
     @FXML 
-    private ChoiceBox<Object> categoria_gui;
+    private ChoiceBox<String> categoria_gui;
     private String[] categoria = {"Popular", "Intermediário", "Luxo"};
     @FXML
-    private ChoiceBox<Object> internet_gui;
+    private ChoiceBox<String> internet_gui;
     private String[] internet = {"Sim", "Não"};
     @FXML
     private Button btn_pesquisar;
@@ -87,7 +90,6 @@ public class ControladorVeiculo {
         String parseMediaKm = mediaKm_gui.getText();
         float mediaKm = Float.parseFloat(parseMediaKm);
 
-        arCondicionado_gui.getItems().addAll(arCondicionado);
         boolean arCondicionado = false;
         String arCondicionadoString = arCondicionado_gui.getValue().toString();
         if (arCondicionadoString.equals("Sim")) {
@@ -118,7 +120,6 @@ public class ControladorVeiculo {
         
         String categoria = categoria_gui.getValue().toString();
         
-        arCondicionado_gui.getItems().addAll(arCondicionado);
         boolean arCondicionado = false;
         String arCondicionadoString = arCondicionado_gui.getValue().toString();
         if (arCondicionadoString.equals("Sim")) {
@@ -127,7 +128,6 @@ public class ControladorVeiculo {
             arCondicionado = false;
         }
 
-        internet_gui.getItems().addAll(internet);
         boolean internet = false;
         String internetString = internet_gui.getValue().toString();
         if (internetString.equals("Sim")) {
@@ -222,5 +222,12 @@ public class ControladorVeiculo {
     public void fecharJanela(ActionEvent event) {
         Stage stage = (Stage) btn_cancelar.getScene().getWindow();
         stage.close();
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        arCondicionado_gui.getItems().addAll(arCondicionado);
+        internet_gui.getItems().addAll(internet);
+        categoria_gui.getItems().addAll(categoria);
     }
 }
